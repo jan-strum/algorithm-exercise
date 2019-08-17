1d5a096635573311a5d0670b36fe02dbd2d221b8
Jan-Strum <janusbased@gmail.com>
2019-08-17 09:38:47 -0500

    missingLetteers unit tests.

---
+++ missingLetters.js
@@ -0,0 +1,10 @@
+const findMissingLetters = str => {
+  if (typeof str !== 'string') {
+    throw new TypeError('findMissingLetters must take a string as an input.')
+  }
+
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  return ''
+}
+
+module.exports = findMissingLetters
--- test/missingLetters-test.js
+++ test/missingLetters-test.js
@@ -1,13 +1,43 @@
 const { expect } = require('chai')
+const findMissingLetters = require('../missingLetters')
 
 describe('findMissingLetters', () => {
-  it('should take a string as an input')
+  it('should take a string as an input', () => {
+    const badInputs = [undefined, null, 1, {}, []]
 
-  it('should return all letters not present in the input string')
+    badInputs.forEach(badInput => {
+      expect(() => findMissingLetters(badInput)).to.throw(TypeError)
+    })
+  })
 
-  it('should return the missing letters in lowercase')
+  const slowFox = 'A slow yellow fox crawls under the proactive dog'
+  const lions = 'Lions, and tigers, and bears, oh my!'
+  const empty = ''
 
-  it('should return the missing letters in alphabetical order')
+  const missingFromSlowFox = findMissingLetters(slowFox)
+  const missingFromLions = findMissingLetters(lions)
+  const missingFromEmpty = findMissingLetters(empty)
 
-  it('should return an empty string when given a pangram')
+  it('should return all letters not present in the input string', () => {
+    expect(missingFromSlowFox.to.equal('bjkmqz'))
+    expect(missingFromLions.to.equal('cfjkpquvwxz'))
+    expect(missingFromEmpty.to.equal('abcdefghijklmnopqrstuvwxyz'))
+  })
+
+  it('should return letters in lowercase', () => {
+    expect(missingFromSlowFox.to.equal(missingFromSlowFox.toLowerCase()))
+    expect(missingFromLions.to.equal(missingFromLions.toLowerCase()))
+    expect(missingFromEmpty.to.equal(missingFromEmpty.toLowerCase()))
+  })
+
+  it('should return letters in alphabetical order', () => {
+    expect(missingFromSlowFox.to.equal(missingFromSlowFox.sort()))
+    expect(missingFromLions.to.equal(missingFromLions.sort()))
+    expect(missingFromEmpty.to.equal(missingFromEmpty.sort()))
+  })
+
+  it('should return an empty string when given a pangram', () => {
+    const pangram = 'A quick brown fox jumps over the lazy dog'
+    expect(findMissingLetters(pangram)).to.equal('')
+  })
 })
