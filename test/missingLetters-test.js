1fa2e40c740cd1e55730cebab1125bee6baf72dc
Jan-Strum <janusbased@gmail.com>
2019-08-17 08:24:49 -0500

    Basic missingLetters tests.

---
+++ test/missingLetters-test.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai')
+
+describe('findMissingLetters', () => {
+  it('should take a string as an input')
+
+  it('should return all letters not present in the input string')
+
+  it('should return the missing letters in lowercase')
+
+  it('should return the missing letters in alphabetical order')
+
+  it('should return an empty string when given a pangram')
+})
