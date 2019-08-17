6bd7ee4d6426f8a98ec8f4804e0001e4d045a432
Jan-Strum <janusbased@gmail.com>
2019-08-17 12:17:44 -0500

    Basic animation-test descriptions.

---
+++ test/animation-test.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai')
+
+describe('animate', () => {
+  it('should take a positive integer representing speed as its first argument')
+
+  it('should take a string of positions as its second argument')
+
+  it('should return an array')
+
+  it(
+    'should return an array, the first element of which represents the inital state of the chamber'
+  )
+
+  it(
+    'should return an array, the last element of which represents the the final, empty state of the chamber'
+  )
+
+  it(
+    'should return an array, each element of which represents the state of the chamber at the corresponding index'
+  )
+})
